c09cbd86603aed874cf50b222ab7dd4a5783d07d
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2017-09-08 22:34:46 +0530

    Clean output

--- conversion.c
+++ conversion.c
@@ -8,10 +8,10 @@ void main() {
     printf("\nEnter value in Kilometers: ");
     scanf("%li", &distance);
 
-    printf("\nValue in Meters: %li", distance*1000);
+    printf("\nValue in Meters     : %li", distance*1000);
     printf("\nValue in Centimeters: %li", distance*1000*100);
-    printf("\nValue in Foot: %li", distance*3280);
-    printf("\nValue in Inches: %li", distance*39370);
+    printf("\nValue in Foot       : %li", distance*3280);
+    printf("\nValue in Inches     : %li", distance*39370);
 
     //getch();
 }
