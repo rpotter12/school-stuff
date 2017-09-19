65b851d229c43a03aa13c22f5eb9d8398825f3d3
ritiek <ritiekmalhotra123@gmail.com>
2017-09-19 21:42:16 +0530

    Write sheet no.

--- acadm/autowrite.py
+++ acadm/autowrite.py
@@ -18,8 +18,12 @@ draw.text((5138, 4962),"17BCS1752",(0,0,0),font=font)
 draw.rectangle(((5096, 5156), (5852, 5348)), fill="white")
 draw.text((5110, 5216),"B.E. CSE",(0,0,0),font=font)
 
+# write sheet no.
+draw.rectangle(((6828, 5032), (7056, 5168)), fill="white")
+draw.text((6838, 5064),"2",(0,0,0),font=font)
+
 # write title
-#draw.rectangle(((5904, 4680), (7056, 4980)), fill="white")
+draw.rectangle(((5904, 4680), (7056, 4980)), fill="white")
 font = ImageFont.truetype("Times_New_Roman.ttf", 99)
 draw.text((5936, 4733),"TITLE OF DRAWING TO",(0,0,0),font=font)
 draw.text((5938, 4839),"BE WRITTEN HERE",(0,0,0),font=font)
