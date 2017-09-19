71458e2b0ff57f6981a33aea877ee95f34237a9f
ritiek <ritiekmalhotra123@gmail.com>
2017-09-19 20:41:38 +0530

    Write course

--- acadm/test.py
+++ acadm/test.py
@@ -7,11 +7,16 @@ draw = ImageDraw.Draw(img)
 font = ImageFont.truetype("Times_New_Roman.ttf", 86)
 
 # write name
-draw.rectangle(((5002, 4668), (5858, 4874)), fill="white")
+draw.rectangle(((5004, 4668), (5858, 4874)), fill="white")
 draw.text((5005, 4740),"RITIEK MALHOTRA",(0,0,0),font=font)
 
 # write UID
 draw.text((5138, 4962),"17BCS1752",(0,0,0),font=font)
+
+# write branch
+draw.text((5110, 5216),"B.E. CSE",(0,0,0),font=font)
+
+# write title
 
 
 img.save('sample-out.jpg')
