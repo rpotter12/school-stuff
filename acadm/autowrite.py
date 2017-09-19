53e2bac781c2c6ec5dec6e5cfed7566f79090b4f
ritiek <ritiekmalhotra123@gmail.com>
2017-09-19 20:54:49 +0530

    Clean existing values before writing

--- acadm/autowrite.py
+++ acadm/autowrite.py
@@ -11,12 +11,15 @@ draw.rectangle(((5004, 4668), (5858, 4874)), fill="white")
 draw.text((5005, 4740),"RITIEK MALHOTRA",(0,0,0),font=font)
 
 # write UID
+draw.rectangle(((5132, 4912), (5856, 5108)), fill="white")
 draw.text((5138, 4962),"17BCS1752",(0,0,0),font=font)
 
 # write branch
+draw.rectangle(((5096, 5156), (5852, 5348)), fill="white")
 draw.text((5110, 5216),"B.E. CSE",(0,0,0),font=font)
 
 # write title
+draw.rectangle(((5904, 4680), (7056, 4980)), fill="white")
 
 
 img.save('sample-out.jpg')
